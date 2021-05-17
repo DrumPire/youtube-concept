08e2bcecc4dbb9e0991fdf7227f9de25aacf213b
Alex <expresproject@gmail.com>
2021-05-17 20:47:59 +0300

    responsive

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 const swiper = new Swiper('.channel-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6
+    },
+    1600: {
+      slidesPerView: 5
+    },
+    1300: {
+      slidesPerView: 4
+    },
+    1100: {
+      slidesPerView: 3
+    },
+    800: {
+      slidesPerView: 2
+    },
+  },
 
   // Navigation arrows
   navigation: {
@@ -9,3 +27,11 @@ const swiper = new Swiper('.channel-slider', {
     prevEl: '.channel-button-prev',
   },
 });
+
+const searchBtn = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open');
+});
+
